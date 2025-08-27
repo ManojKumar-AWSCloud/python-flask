fa734f365f1e964a19bd8b37d2ebd79e1129cc5c
manojkumar.v <manojkumar.v@HJNH6JCXYK-manojkumar.v>
2025-08-27 15:44:17 +0530

    added the new notes

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/home')
 def home():
-    return "Welcome to the Home Page!"
+    return "Welcome to the GitHub Actions with Flask!"
 #app.run(port=5008, debug=True)
